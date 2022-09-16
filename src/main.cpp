1e51f00a3eeca01cd2c70cd80edd660871da884f
Douglas Ward <30300064+dougimus@users.noreply.github.com>
2022-09-16 11:21:13 -0400

    added extension portion and prettied code

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,27 @@
 #include <Arduino.h>
 
 void setup() {
+  // Set serial output speed
   Serial.begin(115200);
+
+  // Hello World
   Serial.println("Hello World!");
+
+  // My name and student #
+  Serial.println("Douglas Ward (000333587)");
+
+  // print blank line
+  Serial.println();
+
+  // Chip and Flash Info
+  Serial.println("ESP8266 Chip ID: " + (String)ESP.getChipId());
+  Serial.println("Flash Chip ID: " + (String)ESP.getFlashChipId());
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() { 
+  // print uptime in milliseconds
+  Serial.println("Uptime: " + (String)millis());
+
+  // wait 2 seconds
+  delay(2000);
 }
